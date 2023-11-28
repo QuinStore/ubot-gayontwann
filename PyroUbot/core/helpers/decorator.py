49ec860748f2d5ce4f8438f0a16cd8458c0552d1
izzy <hitokizzy@gmail.com>
2023-11-28 20:24:07 +0700

    up

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -19,4 +19,4 @@ async def install_my_peer(client):
 async def installPeer():
     tasks = [install_my_peer(client) for client in ubot._ubot]
     await asyncio.gather(*tasks, return_exceptions=True)
-    await bot.send_message(OWNER_ID, "✅ sᴇᴍᴜᴀ ᴘᴇᴇʀ_ɪᴅ ʙᴇʀʜᴀsɪʟ ᴅɪɪɴsᴛᴀʟʟ")
+    await bot.send_message(OWNER_ID, "✅ ʙᴏᴛ ʟᴀɴᴄᴀʀ ᴊᴀʏᴀ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ, ᴊᴀʏᴀ.. ᴊᴀʏᴀ .. ᴊᴀʏᴀᴀᴀ...")
