35800e4fb44a29c30fcaad28198a3048b98c68d3
FakeDEVS <50057075+hitokizzy@users.noreply.github.com>
2023-12-17 21:58:39 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -142,6 +142,7 @@ class Ubot(Client):
                 await self.send_message("me", f"gagal membuat group log, buat group log manual dengan nama group lancarjaya botlog, error : {e}")
                 print(f"user {self.me.id} gagal membuat group log, error : {e}")
         else:
+            pass
             #await self.send_message(log.id, "Lancar Jaya userbot berhasil diaktifkan")
         print(f"- ({me.id}) - STARTED")
 
