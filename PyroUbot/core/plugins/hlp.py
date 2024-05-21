b64210b5335b10546693f055be1a7bd5e59be10d
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-05-21 19:22:48 +0700

    Update hlp.py

--- PyroUbot/core/plugins/hlp.py
+++ PyroUbot/core/plugins/hlp.py
@@ -20,7 +20,7 @@ async def help_cmd(client, message):
                 HELP_COMMANDS[get_arg(message)].__HELP__.format(
                     next((p) for p in prefix)
                 )
-                + "\n<b>© prem botv3</b>",
+                + "\n<b>© GayoWann-Ubot V1 </b>",
                 quote=True,
             )
         else:
