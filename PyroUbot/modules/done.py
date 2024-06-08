add0c139d3fc7b2185890854e8c165a049ede64f
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-08 20:09:25 +0700

    Update done.py

--- PyroUbot/modules/done.py
+++ PyroUbot/modules/done.py
@@ -2,28 +2,26 @@ import asyncio
 import datetime
 from PyroUbot import *
 
-__MODULE__ = "Done"
-__HELP__ = """
- <b>Bantuan Untuk Done</b>
-
-• <b>Perintah</b> : <code>{0}done</code> <b>[name item] [harga] [pembayaran]</b>
-• <b>Penjelasan : konfirmasi pembayaran.</b>
-
-"""
 @PY.UBOT("done")
-async def dunedun(client, message):
-    pler = await message.reply("ᴘʀᴏᴄᴇssɪɴɢ...")
-    await asyncio.sleep(3)
+async def done_command(client, message):
+    wannepep = await message.reply("memproses...")
+    await asyncio.sleep(5)
     try:
-        args = message.text.split(',')
-        if len(args) < 3:
-            await message.reply_text("<b>Usage: .done <name item> <price> <payment></b>")
+        args = message.text.split(" ", 1)
+        if len(args) < 2 or ',' not in args[1]:
+            await message.reply_text("Penggunaan: .done <name item>,<price>,<payment>")
             return
         
-        name_item = args[1]
-        price = args[2]
-        payment = args[3]
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        parts = args[1].split(",", 2)
+        
+        if len(parts) < 2:
+            await message.reply_text("Penggunaan: .done <name item>,<price>,<payment>")
+            return
+        
+        name_item = parts[0].strip()
+        price = parts[1].strip()
+        payment = parts[2].strip() if len(parts) > 2 else "Lainnya"        
+        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         response = (
             f"「 𝗧𝗥𝗔𝗡𝗦𝗔𝗞𝗦𝗜 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 」\n\n"
             f"📦 <b>Barang : {name_item}</b>\n"
@@ -32,7 +30,7 @@ async def dunedun(client, message):
             f"💬 <b>Payment : {payment}</b>\n\n"
             f"𝗧𝗲𝗿𝗶𝗺𝗮𝗸𝗮𝘀𝗶𝗵 𝗧𝗲𝗹𝗮𝗵 𝗢𝗿𝗱𝗲𝗿"
         )
-        await pler.edit(response)
+        await wannepep.edit(response)
     
     except Exception as e:
-        await pler.edit(f"An error occurred: {e}")
+        await wannepep.edit(f"error: {e}")
