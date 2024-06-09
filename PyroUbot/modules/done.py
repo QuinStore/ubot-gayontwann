8e6ed8800268f40d77b6608a6acaba47efd154f5
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-09 14:48:55 +0700

    Update done.py

--- PyroUbot/modules/done.py
+++ PyroUbot/modules/done.py
@@ -6,31 +6,32 @@ __MODULE__ = "done"
 __HELP__ = """
  <b>Bantuan Untuk Done</b>
 
-• <b>Perintah</b> : <code>{0}done</code> <b>[name item],[harga] [pembayaran],[payment]</b>
+• <b>Perintah</b> : <code>{0}done</code> <b>[name item],[harga] [pembayaran]</b>
 • <b>Penjelasan : konfirmasi pembayaran.</b>
 
 """
 
 @PY.UBOT("done")
 async def done_command(client, message):
-    wannepep = await message.reply("memproses...")
+    izzy_ganteng = await message.reply("memproses...")
     await asyncio.sleep(5)
     try:
         args = message.text.split(" ", 1)
         if len(args) < 2 or ',' not in args[1]:
-            await message.reply_text("Penggunaan: .done <name item>,<price>,<payment>")
+            await message.reply_text("Penggunaan: .done name item,price,payment")
             return
         
         parts = args[1].split(",", 2)
         
         if len(parts) < 2:
-            await message.reply_text("Penggunaan: .done <name item>,<price>,<payment>")
+            await message.reply_text("Penggunaan: .done name item,price,payment")
             return
         
         name_item = parts[0].strip()
         price = parts[1].strip()
-        payment = parts[2].strip() if len(parts) > 2 else "Lainnya"        
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        payment = parts[2].strip() if len(parts) > 2 else "Lainnya"
+        
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         response = (
             f"「 𝗧𝗥𝗔𝗡𝗦𝗔𝗞𝗦𝗜 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 」\n\n"
             f"📦 <b>Barang : {name_item}</b>\n"
@@ -39,7 +40,7 @@ async def done_command(client, message):
             f"💬 <b>Payment : {payment}</b>\n\n"
             f"𝗧𝗲𝗿𝗶𝗺𝗮𝗸𝗮𝘀𝗶𝗵 𝗧𝗲𝗹𝗮𝗵 𝗢𝗿𝗱𝗲𝗿"
         )
-        await wannepep.edit(response)
+        await izzy_ganteng.edit(response)
     
     except Exception as e:
-        await wannepep.edit(f"error: {e}")
+        await izzy_ganteng.edit(f"error: {e}")
