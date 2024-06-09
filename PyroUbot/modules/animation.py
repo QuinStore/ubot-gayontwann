15d1b97ece12523f6d224f3fbdf8e4451f35c232
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-10 00:10:39 +0700

    new update feature

---
+++ PyroUbot/modules/animation.py
@@ -0,0 +1,459 @@
+import asyncio
+from time import sleep
+from PyroUbot import *
+
+__MODULE__ = "animasi"
+__HELP__ = """
+ <b>Bantuan Untuk Done</b>
+
+• <b>Perintah</b> : <code>{0}bulan</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}sayang</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}dyno</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}gabut</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}terkadang</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}mf</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}cinta</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+"""
+
+@PY.UBOT("bulan")
+async def bulan(message):
+    animation_interval = 0.2
+    animation_ttl = range(96)
+    await message.edit("wann ganteng..")
+    animation_chars = [
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await message.edit(animation_chars[i % 32])
+
+
+@PY.UBOT("sayang")
+async def wannganteng(message):
+    e = await message.edit("I LOVEE YOUUU 💕")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💘💞💗💕")
+    await e.edit("💘💞💕💗")
+    await e.edit("SAYANG KAMU 💝💖💘")
+    await e.edit("💝💘💓💗")
+    await e.edit("💞💕💗💘")
+    await e.edit("💘💞💕💗")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("SELAMANYA 💕")
+    await e.edit("💘💘💘💘")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("SAYANG")
+    await e.edit("KAMU")
+    await e.edit("I LOVE YOUUUU")
+    await e.edit("MY BABY")
+    await e.edit("💕💞💘💝")
+    await e.edit("💘💕💞💝")
+    await e.edit("SAYANG KAMU💞")
+
+
+@PY.UBOT("dyno")
+async def wanngantengbgt(message):
+    typew = await message.edit("`DIN DINNN.....`")
+    sleep(1)
+    await typew.edit("`DINOOOOSAURUSSSSS!!`")
+    sleep(1)
+    await typew.edit("`🏃                        🦖`")
+    await typew.edit("`🏃                       🦖`")
+    await typew.edit("`🏃                      🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃   `LARII`          🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃WOARGH!   🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                    🦖`")
+    await typew.edit("`🏃                     🦖`")
+    await typew.edit("`🏃  Huh-Huh           🦖`")
+    await typew.edit("`🏃                   🦖`")
+    await typew.edit("`🏃                  🦖`")
+    await typew.edit("`🏃                 🦖`")
+    await typew.edit("`🏃                🦖`")
+    await typew.edit("`🏃               🦖`")
+    await typew.edit("`🏃              🦖`")
+    await typew.edit("`🏃             🦖`")
+    await typew.edit("`🏃            🦖`")
+    await typew.edit("`🏃           🦖`")
+    await typew.edit("`🏃          🦖`")
+    await typew.edit("`🏃         🦖`")
+    await typew.edit("`DIA SEMAKIN MENDEKAT!!!`")
+    sleep(1)
+    await typew.edit("`🏃       🦖`")
+    await typew.edit("`🏃      🦖`")
+    await typew.edit("`🏃     🦖`")
+    await typew.edit("`🏃    🦖`")
+    await typew.edit("`Dahlah Pasrah Aja`")
+    sleep(1)
+    await typew.edit("`🧎🦖`")
+    sleep(2)
+    await typew.edit("`-TAMAT-`")
+
+
+@PY.UBOT("gabut")
+async def wannemangganteng(message):
+    e = await message.edit("`PERNAAHHHHH KAHHH KAUUU MENGIRA`")
+    await e.edit("`SEPEEERTIIIII APAAAA BENTUKKKKKKK CINTAAAA`")
+    await e.edit("`RAMBUUUT WARNAAA WARNII`")
+    await e.edit("`BAGAI GULALI`")
+    await e.edit("`IMUUUTTTTT LUCUUU`")
+    await e.edit("`WALAAUUUU TAK TERLALU TINGGI`")
+    await e.edit("`GW GABUUTTTT`")
+    await e.edit("`EMMMM BACOTNYA`")
+    await e.edit("`GABUTTTT WOI GABUT`")
+    await e.edit("🙈🙈🙈🙈")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("🙈🙈🙈🙈")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("`CILUUUKKK BAAAAA`")
+    await e.edit("🙉🙉🙉🙉")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🚶                                 🐢")
+    await e.edit("`AHHH MANTAP`")
+    await e.edit("🙉")
+    await e.edit("🙈")
+    await e.edit("🙉")
+    await e.edit("🙈")
+    await e.edit("🙉")
+    await e.edit("😂")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("🐢                       🚶")
+    await e.edit("🐢                      🚶")
+    await e.edit("🐢                     🚶")
+    await e.edit("🐢                    🚶")
+    await e.edit("🐢                   🚶")
+    await e.edit("🐢                  🚶")
+    await e.edit("🐢                 🚶")
+    await e.edit("🐢                🚶")
+    await e.edit("🐢               🚶")
+    await e.edit("🐢              🚶")
+    await e.edit("🐢             🚶")
+    await e.edit("🐢            🚶")
+    await e.edit("🐢           🚶")
+    await e.edit("🐢          🚶")
+    await e.edit("🐢         🚶")
+    await e.edit("🐢        🚶")
+    await e.edit("🐢       🚶")
+    await e.edit("🐢      🚶")
+    await e.edit("🐢     🚶")
+    await e.edit("🐢    🚶")
+    await e.edit("🐢   🚶")
+    await e.edit("🐢  🚶")
+    await e.edit("🐢 🚶")
+    await e.edit("🐢🚶")
+    await e.edit("🚶🐢")
+    await e.edit("🚶 🐢")
+    await e.edit("🚶  🐢")
+    await e.edit("🚶   🐢")
+    await e.edit("🚶    🐢")
+    await e.edit("🚶     🐢")
+    await e.edit("🚶      🐢")
+    await e.edit("🚶       🐢")
+    await e.edit("🚶        🐢")
+    await e.edit("🚶         🐢")
+    await e.edit("🚶          🐢")
+    await e.edit("🚶           🐢")
+    await e.edit("🚶            🐢")
+    await e.edit("🚶             🐢")
+    await e.edit("🚶              🐢")
+    await e.edit("🚶               🐢")
+    await e.edit("🚶                🐢")
+    await e.edit("🚶                 🐢")
+    await e.edit("🚶                  🐢")
+    await e.edit("🚶                   🐢")
+    await e.edit("🚶                    🐢")
+    await e.edit("🚶                     🐢")
+    await e.edit("🚶                      🐢")
+    await e.edit("🚶                       🐢")
+    await e.edit("🚶                        🐢")
+    await e.edit("🚶                         🐢")
+    await e.edit("🚶                          🐢")
+    await e.edit("🚶                           🐢")
+    await e.edit("🚶                            🐢")
+    await e.edit("🚶                             🐢")
+    await e.edit("🚶                              🐢")
+    await e.edit("🚶                               🐢")
+    await e.edit("🚶                                🐢")
+    await e.edit("`GABUT`")
+
+
+@PY.UBOT("terkadang")
+async def wannpalingganteng(message):
+    typew = await message.edit("`Terkadang`")
+    sleep(1)
+    await typew.edit("`Mencintai Seseorang`")
+    sleep(1)
+    await typew.edit("`Hanya Akan Membuang Waktumu`")
+    sleep(1)
+    await typew.edit("`Ketika Waktumu Habis`")
+    sleep(1)
+    await typew.edit("`Tambah Aja 5000`")
+    sleep(1)
+    await typew.edit("`Bercanda`")
+
+
+# Create by myself @localheart
+
+
+@PY.UBOT("mf")
+async def wanngantengsekali(message):
+    await message.edit( "`mf g dl` **ミ(ノ;_ _)ノ=3** ")
+
+
+@PY.UBOT("cinta")
+async def wannemangpalingganteng(message):
+    animation_chars = [
+            "`Connecting Ke Server Cinta`",
+            "`Mencari Target Cinta`",
+            "`Mengirim Cintaku.. 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Mengirim Cintaku.. 84%\n█████████████████████▒▒▒▒ `",
+            "`Mengirim Cintaku.. 100%\n█████████CINTAKU███████████ `",
+            "`Cintaku Sekarang Sepenuhnya Terkirim Padamu, Dan Sekarang Aku Sangat Mencintai Mu, I Love You 💞`",
+    ]
+    animation_interval = 2
+    animation_ttl = range(11)
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await message.edit(animation_chars[i % 11])
---
+++ PyroUbot/modules/animation3.py
@@ -0,0 +1,406 @@
+import asyncio
+from time import sleep
+from PyroUbot import *
+
+__MODULE__ = "animasi 3"
+__HELP__ = """
+ <b>Bantuan Untuk Animasi 3</b>
+
+• <b>Perintah</b> : <code>{0}tank</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}babi</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}anjing</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}bernyanyi</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+• <b>Perintah</b> : <code>{0}santet</code>
+• <b>Penjelasan : gatau gabut doang.</b>
+
+"""
+
+@PY.UBOT("tank")
+async def _(message):
+    await message.edit(
+        "█۞███████]▄▄▄▄▄▄▄▄▄▄▃ \n"
+        "▂▄▅█████████▅▄▃▂…\n"
+        "[███████████████████]\n"
+        "◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤\n",
+    )
+
+
+@PY.UBOT("babi")
+async def _(message):
+    await message.edit(
+        "┈┈┏━╮╭━┓┈╭━━━━╮\n"
+        "┈┈┃┏┗┛┓┃╭┫Ngok ┃\n"
+        "┈┈╰┓▋▋┏╯╯╰━━━━╯\n"
+        "┈╭━┻╮╲┗━━━━╮╭╮┈\n"
+        "┈┃▎▎┃╲╲╲╲╲╲┣━╯┈\n"
+        "┈╰━┳┻▅╯╲╲╲╲┃┈┈┈\n"
+        "┈┈┈╰━┳┓┏┳┓┏╯┈┈┈\n"
+        "┈┈┈┈┈┗┻┛┗┻┛┈┈┈┈\n",
+    )
+
+
+@PY.UBOT("anjing")
+async def _(message):
+    await message.edit(
+        "╥━━━━━━━━╭━━╮━━┳\n"
+        "╢╭╮╭━━━━━┫┃▋▋━▅┣\n"
+        "╢┃╰┫┈┈┈┈┈┃┃┈┈╰┫┣\n"
+        "╢╰━┫┈┈┈┈┈╰╯╰┳━╯┣\n"
+        "╢┊┊┃┏┳┳━━┓┏┳┫┊┊┣\n"
+        "╨━━┗┛┗┛━━┗┛┗┛━━┻\n",
+    )
+
+
+@PY.UBOT("bernyanyi")
+async def _(message):
+    typew = await message.edit("**Ganteng Doang Gak Bernyanyi (ง˙o˙)ว**")
+    sleep(2)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+    sleep(1)
+    await typew.edit("**♪┏(・o･)┛♪┗ ( ･o･) ┓**")
+    sleep(1)
+    await typew.edit("**♪┗ ( ･o･) ┓♪┏ (・o･) ┛♪**")
+
+
+@PY.UBOT("santet")
+async def _(message):
+    typew = await message.edit("`Mengaktifkan Perintah Santet Online....`")
+    sleep(2)
+    await typew.edit("`Mencari Nama Orang Ini...`")
+    sleep(1)
+    await typew.edit("`Santet Online Segera Dilakukan`")
+    sleep(1)
+    await typew.edit("0%")
+    number = 1
+    await typew.edit(str(number) + "%   ▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   █████████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ██████████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████▊")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ███████████████▉")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████████")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████████▎")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████████▍")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████████▌")
+    number += 1
+    sleep(0.03)
+    await typew.edit(str(number) + "%   ████████████████▌")
+    sleep(1)
+    await typew.edit("`Target Berhasil Tersantet Online 🥴`")
