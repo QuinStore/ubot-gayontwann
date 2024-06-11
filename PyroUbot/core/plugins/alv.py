3ca46a46b0e25f21d7337c1de75c25fa0b9d3824
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-11 15:10:21 +0700

    Update alv.py

--- PyroUbot/core/plugins/alv.py
+++ PyroUbot/core/plugins/alv.py
@@ -35,11 +35,11 @@ async def alive_query(client, inline_query):
             get_exp = await get_expired_date(my.me.id)
             exp = get_exp.strftime("%d-%m-%Y")
             if my.me.id == OWNER_ID:
-                status = "GayoWann-Ubot V1 <code>[ꜰᴏᴜɴᴅᴇʀ]</code>"
+                status = "ɢᴀʏᴏᴡᴀɴɴ-ᴜʙᴏᴛ ᴠ1  <code>[ꜰᴏᴜɴᴅᴇʀ]</code>"
             elif my.me.id in await get_seles():
-                status = "GayoWann-Ubot V1 </b> <code>[ᴀᴅᴍɪɴ]</code>"
+                status = "ɢᴀʏᴏᴡᴀɴɴ-ᴜʙᴏᴛ ᴠ1  </b> <code>[ᴀᴅᴍɪɴ]</code>"
             else:
-                status = "GayoWann-Ubot V1 </b>"
+                status = "ɢᴀʏᴏᴡᴀɴɴ-ᴜʙᴏᴛ ᴠ1  </b>"
             button = Button.alive(get_id)
             start = datetime.now()
             await my.invoke(Ping(ping_id=0))
