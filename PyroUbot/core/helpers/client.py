afaf00cf5068599d2364ba954803a9b31d4d024b
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-13 21:47:09 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -34,6 +34,26 @@ class PY:
 
         return wrapper
 
+    @staticmethod
+    def AFK(afk_no):
+        def wrapper(func):
+            afk_check = (
+                (filters.mentioned | filters.private)
+                & ~filters.bot
+                & ~filters.me
+                & filters.incoming
+                if afk_no
+                else filters.me & ~filters.incoming
+            )
+
+            @ubot.on_message(afk_check, group=10)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+        
     def INLINE(command):
         def wrapper(func):
             @bot.on_inline_query(filters.regex(command))
