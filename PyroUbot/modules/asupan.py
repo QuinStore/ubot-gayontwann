47dbfcc2a6dbcdf40fe2b10029e952e7f50393f5
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-14 10:09:21 +0700

    Update asupan.py

--- PyroUbot/modules/asupan.py
+++ PyroUbot/modules/asupan.py
@@ -13,11 +13,6 @@ __HELP__ = """
   <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}cowok</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘʜᴏᴛᴏ ᴄᴏᴡᴏᴋ ʀᴀɴᴅᴏᴍ
   
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}bokep</code>
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴠɪᴅᴇᴏ ʀᴀɴᴅᴏᴍ
-  
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}anime</code>
-  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴠɪᴅᴇᴏ ʀᴀɴᴅᴏᴍ
 """
 
 @PY.UBOT("asupan")
@@ -66,41 +61,3 @@ async def photo_cowok(client, message):
         await y.delete()
     except Exception as error:
         await y.edit(error)
-
-@PY.UBOT("anime")        
-async def photo_anime(client, message):
-    y = await message.reply_text(emoji("proses") + f"**mencari anime...**", quote=True)
-    anime_channel = random.choice(["@animehikarixa", "@anime_WallpapersHD"])
-    try:
-        animenya = []
-        async for anime in client.search_messages(
-            anime_channel, filter=MessagesFilter.PHOTO
-        ):
-            animenya.append(anime)
-        photo = random.choice(animenya)
-        await photo.copy(message.chat.id, reply_to_message_id=message.id)
-        await y.delete()
-    except Exception as error:
-        await y.edit(error)
-
-@PY.UBOT("bokep")
-async def video_bokep(client, message):
-    y = await message.reply_text(emoji("proses") + f"**mencari video bokep**...", quote=True)
-    try:
-        await client.join_chat("https://t.me/+kJJqN5kUQbs1NTVl")
-    except:
-        pass
-    try:
-        bokepnya = []
-        async for bokep in client.search_messages(
-            -1001867672427, filter=MessagesFilter.VIDEO
-        ):
-            bokepnya.append(bokep)
-        video = random.choice(bokepnya)
-        await video.copy(message.chat.id, reply_to_message_id=message.id)
-        await y.delete()
-    except Exception as error:
-        await y.edit(error)
-    if client.me.id == OWNER_ID:
-        return
-    await client.leave_chat(-1001867672427)
