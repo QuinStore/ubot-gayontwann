dc59c6a20753ed3bf653aeb34e9f1d5ae3f6bb67
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-06-14 08:06:13 +0700

    Update texttoaudio.py

--- PyroUbot/modules/texttoaudio.py
+++ PyroUbot/modules/texttoaudio.py
@@ -1,19 +1,19 @@
-from userbot import *
+from PyroUbot import *
 
 
 __MODULE__ = "ᴀᴜᴅɪᴏ"
 __HELP__ = f"""
 <b>『 bantuan untuk text to audio 』</b>
 
-  <b>• perintah:</b> <code>{PREFIX[0]}tts</code> [reply/text]
+  <b>• perintah:</b> <code>.tts</code> [reply/text]
   <b>• penjelasan:</b> untuk merubah tect menjadi menjadi pesan suara
 
-  <b>• perintah:</b> <code>{PREFIX[0]}bahasa</code>
+  <b>• perintah:</b> <code>.bahasa</code>
   <b>• penjelasan:</b> untuk merubah bahasa text to audio
 
   note : atur bahasa daulu untuk menggunakan fitur ini
 """
 
-@CB.UBOT("tts", sudo=False)
+@PY.UBOT("tts")
 async def _(client, message):
     await tts_cmd(client, message)
