d350283ec46255b1073098440808f50bae24c037
Dante <148047531+Vooyage21@users.noreply.github.com>
2024-06-20 01:05:26 +0700

    update PyroUbot/core/database/logger.py

--- PyroUbot/core/database/logger.py
+++ PyroUbot/core/database/logger.py
@@ -8,6 +8,18 @@ async def get_log_group(user_id: int) -> bool:
         return None
     return cek["logger"]
 
+async def get_botlog(user_id: int):
+    user_data = await logdb.users.find_one({"user_id": user_id})
+    botlog_chat_id = user_data.get("bot_log_group_id") if user_data else None
+    return botlog_chat_id
+
+
+async def set_botlog(user_id: int, botlog_chat_id: int):
+    await logdb.users.update_one(
+        {"user_id": user_id},
+        {"$set": {"bot_log_group_id": botlog_chat_id}},
+        upsert=True,
+    )
 
 async def set_log_group(user_id: int, logger):
     cek = await get_log_group(user_id)
